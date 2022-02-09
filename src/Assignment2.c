ea41bf3980a99a32ecc852d1a8d71a98f95f9966
Niklas-Mezynski <n.mezynski@student.fontys.nl>
2022-02-09 16:30:09 +0100

    .

--- src/Assignment2.c
+++ src/Assignment2.c
@@ -3,15 +3,25 @@
 void sumArray(int arr[], int size, int *result);
 
 int main() {
-    int arr[5] = {1, 2, 3, 4, 5}; // Sum should be 15
+    int arr[5] = {1, 2, 3, 4, 5};
     int arraySize = sizeof(arr) / sizeof(arr[0]);
-    int result = 0;
+    int result;
     sumArray(arr, arraySize, &result);
-    printf("The sum of the array is: %d", result);
+    printf("Scenario 1 - The sum of the array is: %d\n", result); // Sum should be 15
+
+    arr[0] = 6;
+    sumArray(arr, arraySize, &result);
+    printf("Scenario 2 - The sum of the array is: %d\n", result); // Sum should be 20
+
+    int arr2[1] = {0};
+    sumArray(arr2, 1, &result);
+    printf("Scenario 2 - The sum of the array is: %d\n", result); // Sum should be 0
+
     return 0;
 }
 
 void sumArray(int arr[], int size, int *result) {
+    *result = 0;
     for (int i = 0; i < size; i++)
     {
         *result += arr[i];
