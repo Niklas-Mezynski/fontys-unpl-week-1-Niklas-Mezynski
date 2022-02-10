b44f65e933cffc130ca2416280895b0031c2c5e8
Niklas-Mezynski <70883487+Niklas-Mezynski@users.noreply.github.com>
2022-02-10 17:28:34 +0100

    Week 1

--- src/Assignment4.c
+++ src/Assignment4.c
@@ -2,7 +2,8 @@
 
 void convertToIndexes(char in1, int in2, int *out1, int *out2);
 
-int main() {
+int main()
+{
     char in1;
     int in2, out1, out2;
     in1 = 'F';
@@ -19,11 +20,26 @@ int main() {
     in2 = 8;
     convertToIndexes(in1, in2, &out1, &out2);
     printf("Scenario 3 - %c,%d converts to %d,%d\n", in1, in2, out1, out2);
+    in1 = 'I';
+    in2 = 9;
+    convertToIndexes(in1, in2, &out1, &out2);
+    printf("Scenario 4 - %c,%d converts to %d,%d\n", in1, in2, out1, out2);
     return 0;
 }
 
-void convertToIndexes(char in1, int in2, int *out1, int *out2) {
-    *out1 = in1 - 'A';
-    *out2 = in2 - 1;
+void convertToIndexes(char in1, int in2, int *out1, int *out2)
+{
+    if (in1 >= 'A' && in1 <= 'H')
+    {
+        *out1 = in1 - 'A';
+    } else {
+        *out1 = 0;
+    }
 
+    if (in2 >= 1 && in2 <= 8)
+    {
+        *out2 = in2 - 1;
+    } else {
+        *out2 = 0;
+    }
 }
--- src/Assignment5.c
+++ src/Assignment5.c
@@ -1,42 +1,85 @@
 #include "stdio.h"
 
 unsigned long int date_encode(int year, int month, int day);
-void printBits(size_t const size, void const * const ptr);
+int date_decode(unsigned long int input, int *year, int *month, int *day);
+void printInBinary(unsigned long int number);
 
-int main() {
-    unsigned long int encoded_date = date_encode(4095, 12, 31);
-    printBits(sizeof(encoded_date), &encoded_date);
+int main()
+{
+    unsigned long int encoded_date;
+    int y, m, d;
+    y = 4095;
+    m = 12;
+    d = 31;
+    encoded_date = date_encode(y, m, d);
+    printf("Scenario 1 - Year: %d   Month: %d   Day: %d encodes to: ", y, m, d);
+    printInBinary(encoded_date);
+
+    y = 2022;
+    m = 2;
+    d = 10;
+    encoded_date = date_encode(y, m, d);
+    printf("Scenario 2 - Year: %d   Month: %d   Day: %d encodes to: ", y, m, d);
+    printInBinary(encoded_date);
+
+    y = -1;
+    m = 13;
+    d = 10;
+    encoded_date = date_encode(y, m, d);
+    printf("Scenario 3 - Year: %d   Month: %d   Day: %d encodes to: ", y, m, d);
+    printInBinary(encoded_date);
+
+    y,m,d = 0;
+    date_decode(2097055, &y, &m, &d);
+    printf("Decoding int 2097055 [00000000000111111111111110011111] -> Year: %d   Month: %d   Day: %d \n", y, m, d);
+
+    y,m,d = 0;
+    date_decode(1035338, &y, &m, &d);
+    printf("Decoding int 1035338 [00000000000011111100110001001010] -> Year: %d   Month: %d   Day: %d \n", y, m, d);
     return 0;
 }
 
-unsigned long int date_encode(int year, int month, int day) {
+unsigned long int date_encode(int year, int month, int day)
+{
     unsigned long int result;
-    result <<= 12;
-    if (year < 4096) {
+    //checking that none of the values are negative
+    if (year < 0 || month < 0 || day < 0)
+    {
+        return 0;
+    }
+
+    // result <<= 12;
+    //Adding the integers to the result and then shifting the bits to the left afterwards
+    if (year < 4096)
+    {
         result += year;
     }
     result <<= 4;
-    if (month <= 12) {
+    if (month <= 12)
+    {
         result += month;
     }
     result <<= 5;
-    if (day <= 31) {
+    if (day <= 31)
+    {
         result += day;
     }
     return result;
 }
 
-void printBits(size_t const size, void const * const ptr)
+int date_decode(unsigned long int input, int *year, int *month, int *day) {
+    //Shifting only bits filled with one to the proper position
+    //Then using the & operator to get the right bits from input
+    //Then shifting it back to its proper position
+    *year = ((4095 << 9) & input) >> 9;
+    *month = ((15 << 5) & input) >> 5;
+    *day = (31) & input;
+}
+
+void printInBinary(unsigned long int number)
 {
-    unsigned char *b = (unsigned char*) ptr;
-    unsigned char byte;
-    int i, j;
-    
-    for (i = size-1; i >= 0; i--) {
-        for (j = 7; j >= 0; j--) {
-            byte = (b[i] >> j) & 1;
-            printf("%u", byte);
-        }
-    }
-    puts("");
+    unsigned long int i;
+    for (i = 1 << 31; i > 0; i = i / 2)
+        (number & i) ? printf("1") : printf("0");
+    printf("\n");
 }
